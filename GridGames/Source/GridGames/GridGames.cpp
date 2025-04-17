10bca096ea7f024829ab0c937ca6c4f608d700e3
Huw Weaver <huwweaver@gmail.com>
2025-04-17 08:36:11 +0100

    project creation

---
+++ GridGames/Source/GridGames/GridGames.cpp
@@ -0,0 +1,9 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "GridGames.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, GridGames, "GridGames" );
+
+DEFINE_LOG_CATEGORY(LogGridGames)
+ 
---
+++ GridGames/Source/GridGames/GridGamesGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "GridGamesGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AGridGamesGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AGridGamesGameMode();
+};
+
+
+
