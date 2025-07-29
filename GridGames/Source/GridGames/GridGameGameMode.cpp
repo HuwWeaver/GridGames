616ae2495c5fd3ad8d6d6e4c3741b27437e54ba0
Huw Weaver <huwweaver@gmail.com>
2025-07-29 12:52:34 +0100

    GridGameGameMode fully commented

--- GridGames/Source/GridGames/GridGameGameMode.cpp
+++ GridGames/Source/GridGames/GridGameGameMode.cpp
@@ -15,6 +15,7 @@ void AGridGameGameMode::BeginPlay()
 	CreateGrid();
 }
 
+// Creates the grid of tiles using specified AGridTile class and specified grid dimensions.
 void AGridGameGameMode::CreateGrid()
 {
 	//TODO: Remove LogTemp log, implement more robust method
@@ -40,6 +41,9 @@ void AGridGameGameMode::CreateGrid()
 	PopulateBoard();
 }
 
+// Populates the grid using the specified SetupData and MovementData DataTables with the specified GamePiece class.
+// The SetupData DataTable contains the initial setup properties for each piece, such as starting coordinates and team color.
+// The MovementData DataTable contains the movement properties for each piece, such as movement vectors and range limits.
 void AGridGameGameMode::PopulateBoard()
 {
 	//TODO: Remove LogTemp log, implement more robust method
@@ -54,7 +58,7 @@ void AGridGameGameMode::PopulateBoard()
 	{
 		FPieceSetupProperties* Row = PiecesSetupData->FindRow<FPieceSetupProperties>(RowName, "");
 
-		//X and Y are swapped for first vector as grid X axis doesn't follow world X axis, but rather follows world Y (and vice versa).
+		// X and Y are swapped for first vector as grid X axis doesn't follow world X axis, but rather follows world Y (and vice versa).
 		FVector Location = FVector(Row->StartingCoordinates.Y * TileSize, Row->StartingCoordinates.X * TileSize, 0.0) + FVector(TileSize / 2, TileSize / 2, 100);
 		FRotator Rotation = Row->bWhite ? FRotator(0,0,0) : FRotator(0,180,0);
 		FActorSpawnParameters SpawnInfo;
@@ -78,20 +82,27 @@ void AGridGameGameMode::PopulateBoard()
 	UE_LOG(LogTemp, Display, TEXT("Board Populated!"));
 }
 
+// Step Moves are moves with a single target tile
 void AGridGameGameMode::StepMove(AGamePiece* Piece, const FPieceMovementProperties& Move)
 {	
+	// Get the movement vector and adjust it based on the piece's team color (white or black).
 	FVector MovementVector = Move.MovementVector;
 	if (!Piece->GetSetupProperties().bWhite) MovementVector *= -1;
 
+	// Get the target coordinate by adding the movement vector to the piece's current coordinate.
 	FVector TargetCoordinate = Piece->GetCurrentCoordinate() + MovementVector;
 
+	// Check if the target coordinate is within the bounds of the grid.
 	if (!GridMap.Contains(TargetCoordinate))
 	{
 		//Target Tile is out of bounds = Invalid Move
 		return;
 	}
 
+	// Get the target tile from the grid map using the target coordinate.
 	AGridTile& TargetTile = *GridMap.FindRef(TargetCoordinate);
+
+	// Check if the target tile is occupied by another piece.
 	if (!TargetTile.GetOccupied())
 	{
 		//Empty Tile = Valid Move
@@ -99,13 +110,15 @@ void AGridGameGameMode::StepMove(AGamePiece* Piece, const FPieceMovementProperti
 		MovedPieces.Add(Piece);
 		TArray<FVector> TargetCoordinates;
 		TargetCoordinates.Add(TargetCoordinate);
-		
+
+		// Add the target coordinate to the list of valid move destinations and outcomes.
 		ValidMoveDestinations.Add(TargetCoordinate);
 		ValidMoveOutcomes.Add(TargetCoordinate, FMoveOutcome(MovedPieces, TargetCoordinates));
 
 		return;
 	}
 
+	// If the target tile is occupied, get the piece occupying it.
 	AGamePiece* OccupyingPiece = TargetTile.GetOccupyingPiece();
 	if (OccupyingPiece == nullptr)
 	{
@@ -114,12 +127,14 @@ void AGridGameGameMode::StepMove(AGamePiece* Piece, const FPieceMovementProperti
 		return;
 	}
 
+	// Check if the occupying piece is on the same team as the moving piece.
 	if (OccupyingPiece->GetSetupProperties().bWhite == Piece->GetSetupProperties().bWhite)
 	{
 		//Tile Occupied by Same Team = Invalid Move
 		return;
 	}
 
+	// Check if the move can capture an opposing piece.
 	if (Move.bCanCapture)
 	{
 		//Tile occupied by opposing team && Move can capture = Valid Move
@@ -130,6 +145,7 @@ void AGridGameGameMode::StepMove(AGamePiece* Piece, const FPieceMovementProperti
 		TArray<AGamePiece*> CapturedPieces;
 		CapturedPieces.Add(OccupyingPiece);
 
+		// Add the target coordinate to the list of valid move destinations and outcomes.
 		ValidMoveDestinations.Add(TargetCoordinate);
 		ValidMoveOutcomes.Add(TargetCoordinate, FMoveOutcome(MovedPieces, TargetCoordinates, CapturedPieces));
 		return;
@@ -139,18 +155,24 @@ void AGridGameGameMode::StepMove(AGamePiece* Piece, const FPieceMovementProperti
 	return;
 }
 
+// Range Moves are moves with a movement vector that can be applied multiple times, up to a range limit, to reach multiple target tiles.
 void AGridGameGameMode::RangeMove(AGamePiece* Piece, const FPieceMovementProperties& Move, const int& RangeLimit)
 {
+	// Get the movement vector and adjust it based on the piece's team color (white or black).
 	FVector MovementVector = Move.MovementVector;
 	if (!Piece->GetSetupProperties().bWhite) MovementVector *= -1;
 
+	// Get the target coordinate by adding the movement vector to the piece's current coordinate, then reduce the remaining range.
 	FVector TargetCoordinate = Piece->GetCurrentCoordinate() + MovementVector;
 	int RemainingRange = RangeLimit - 1;
 
+	// Check if the target coordinate is within the bounds of the grid.
 	while (GridMap.Contains(TargetCoordinate))
 	{
+		// Get the target tile from the grid map using the target coordinate.
 		AGridTile& TargetTile = *GridMap.FindRef(TargetCoordinate);
 
+		// Check if the target tile is occupied by another piece.
 		if (!TargetTile.GetOccupied())
 		{
 			//Empty Tile = Valid Move
@@ -159,17 +181,19 @@ void AGridGameGameMode::RangeMove(AGamePiece* Piece, const FPieceMovementPropert
 			TArray<FVector> TargetCoordinates;
 			TargetCoordinates.Add(TargetCoordinate);
 
+			// Add the target coordinate to the list of valid move destinations and outcomes.
 			ValidMoveDestinations.Add(TargetCoordinate);
 			ValidMoveOutcomes.Add(TargetCoordinate, FMoveOutcome(MovedPieces, TargetCoordinates));
 
+			// Check if the range limit is -99 (indicating no range limit).
 			if (RangeLimit == -99)
 			{
-				//No Range Limit Specified - Boundless Range
 				//Move to Next Tile
 				TargetCoordinate = TargetCoordinate + MovementVector;
 				continue;
 			}
 
+			// Check if the remaining range is less than or equal to 0, in which case the range limit has been reached.
 			if (RemainingRange <= 0)
 			{
 				//Range Limit Reached
@@ -184,6 +208,7 @@ void AGridGameGameMode::RangeMove(AGamePiece* Piece, const FPieceMovementPropert
 			}
 		}
 
+		// If the target tile is occupied, check the piece occupying it.
 		AGamePiece* OccupyingPiece = TargetTile.GetOccupyingPiece();
 		if (OccupyingPiece == nullptr)
 		{
@@ -192,12 +217,14 @@ void AGridGameGameMode::RangeMove(AGamePiece* Piece, const FPieceMovementPropert
 			return;
 		}
 
+		// Check if the occupying piece is on the same team as the moving piece.
 		if (OccupyingPiece->GetSetupProperties().bWhite == Piece->GetSetupProperties().bWhite)
 		{
 			//Tile Occupied by Same Team = Invalid Move
 			return;
 		}
 
+		// Check if the move can capture an opposing piece.
 		if (Move.bCanCapture)
 		{
 			//Tile occupied by opposing team && Move can capture = Valid Move
@@ -208,6 +235,7 @@ void AGridGameGameMode::RangeMove(AGamePiece* Piece, const FPieceMovementPropert
 			TArray<AGamePiece*> CapturedPieces;
 			CapturedPieces.Add(OccupyingPiece);
 
+			// Add the target coordinate to the list of valid move destinations and outcomes.
 			ValidMoveDestinations.Add(TargetCoordinate);
 			ValidMoveOutcomes.Add(TargetCoordinate, FMoveOutcome(MovedPieces, TargetCoordinates, CapturedPieces));
 			return;
@@ -218,10 +246,14 @@ void AGridGameGameMode::RangeMove(AGamePiece* Piece, const FPieceMovementPropert
 	}
 }
 
+// This function is a placeholder for other types of moves that may be implemented in derived classes - usually for special moves like castling or en passant in chess.
 void AGridGameGameMode::OtherMove(AGamePiece* Piece, const FPieceMovementProperties& Move)
 {
 }
 
+// This function attempts to move a game piece to a target tile, checking if the move is valid based on the valid move destinations and outcomes.
+// If the target tile is not in the valid move destinations or outcomes, it logs an error and returns without moving the piece.
+// If the move is valid, it retrieves the move outcome and moves the piece(s) accordingly, capturing any pieces that are part of the outcome, before deselecting the piece and logging the completed move.
 void AGridGameGameMode::TryMovePiece(AGamePiece* Piece, AGridTile* TargetTile)
 {
 	if (!ValidMoveDestinations.Contains(TargetTile->GetCoordinates()))
@@ -259,6 +291,9 @@ void AGridGameGameMode::TryMovePiece(AGamePiece* Piece, AGridTile* TargetTile)
 	PieceDeselected();
 }
 
+// This function is called when a game piece is selected.
+// It retrieves the movement data for the selected piece and calculates all valid move destinations based on the piece's movement properties.
+// It then shows the valid move tiles on the grid by calling ShowValidMove(true) on each valid tile.
 void AGridGameGameMode::PieceSelected(AGamePiece* Piece)
 {
 	FPieceMovementData MovementData = Piece->GetMovementData();
@@ -298,6 +333,8 @@ void AGridGameGameMode::PieceSelected(AGamePiece* Piece)
 	}
 }
 
+// This function is called when a game piece is deselected.
+// It hides all valid move tiles for that game piece by calling ShowValidMove(false) on each valid tile and clears the list of valid move destinations.
 void AGridGameGameMode::PieceDeselected()
 {
 	for (const FVector& TileCoordinate : ValidMoveDestinations)
@@ -311,8 +348,8 @@ void AGridGameGameMode::PieceDeselected()
 	ValidMoveDestinations.Empty();
 }
 
+// This function is a placeholder for derived classes to implement specific promotion logic, such as providing a choice of piece to promote to.
 void AGridGameGameMode::OnTriggerPromotion(AGamePiece* Piece)
 {
-	//To be implemented in derived classes
 	UE_LOG(LogTemp, Warning, TEXT("OnTriggerPromotion called for Piece: %s"), *Piece->GetPieceName().ToString());
 }
